37e989f8670731502de4cfe6b422ccd702f12d05
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-08-03 02:25:57 +0200

    build: expose useful headers

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ if (!API_KEY) {
   throw new Error("Environment variable `API_KEY` can't be empty")
 }
 
+const HEADERS = [
+  'access-control-expose-headers',
+  'x-response-time',
+  'x-pricing-plan',
+  'x-cache-ttl',
+  'x-fetch-mode',
+  'x-cache-status',
+  'x-fetch-time',
+  'x-fetch-date',
+  'x-cache-expired-at'
+]
+
 const CACHE = {}
 
 const trustedDomains = DOMAINS.split(',').map(n => n.trim())
@@ -23,6 +35,7 @@ const isTrustedDomain = origin =>
 
 module.exports = (req, res) => {
   if (!isTrustedDomain(req.headers.origin)) return send(res, 401)
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
 
   get(
     {
@@ -31,6 +44,10 @@ module.exports = (req, res) => {
         'x-api-key': API_KEY
       }
     },
-    (error, stream) => (error ? sendError(req, res, error) : stream.pipe(res))
+    (error, stream) => {
+      if (error) return sendError(req, res, error)
+      HEADERS.forEach(header => res.setHeader(header, stream.headers[header]))
+      stream.pipe(res)
+    }
   )
 }
