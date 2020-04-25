c593f46038494b7b115968695e89490b69b9a69c
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-25 22:54:32 +0200

    build: add Access-Control-Allow-Origin header

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ const debug = require('debug-logfmt')('microlink-proxy')
 const { getDomain } = require('tldts')
 const mql = require('@microlink/mql')
 const { promisify } = require('util')
-const { send } = require('micri')
+const createCors = require('cors')
 const stream = require('stream')
-
 const pipeline = promisify(stream.pipeline)
 
 const REQUIRED_ENVS = ['DOMAINS', 'API_KEY']
-
-const CACHE = Object.create(null)
 
 const missing = REQUIRED_ENVS.filter(key => process.env[key] == null)
 
@@ -19,25 +16,11 @@ if (missing.length > 0) {
   throw new Error(`Missing required environment variable(s): ${missing.join(', ')}`)
 }
 
-const trustedDomains = process.env.DOMAINS.split(',').map(n => n.trim())
-
-const isTrustedDomain = origin =>
-  CACHE[origin] || (CACHE[origin] = trustedDomains.includes(getDomain(origin)))
-
-const verifyDomain = (req, res) => {
-  debug('headers', req.headers)
-  const origin = req.headers.origin || req.headers['x-forwarded-host']
-  const isTrusted = isTrustedDomain(origin)
-  debug({ origin, isTrusted })
-  if (!isTrustedDomain(origin)) return send(res, 401)
-  res.setHeader('Access-Control-Allow-Origin', origin)
-}
+const whitelistDomains = process.env.DOMAINS.split(',').map(n => n.trim())
 
 const toSearchParams = req => new URL(req.url, 'http://localhost').searchParams
 
-module.exports = (req, res) => {
-  verifyDomain(req, res)
-
+const proxy = (req, res) => {
   const stream = mql.stream('https://pro.microlink.io', {
     searchParams: toSearchParams(req),
     headers: {
@@ -48,3 +31,13 @@ module.exports = (req, res) => {
 
   pipeline(stream, res)
 }
+
+const cors = createCors({
+  origin: (origin, cb) => {
+    const isTrusted = whitelistDomains.includes(getDomain(origin))
+    debug({ origin, isTrusted })
+    return cb(null, isTrusted)
+  }
+})
+
+module.exports = (req, res) => cors(req, res, () => proxy(req, res))
